d1bbb965f3de9128519fb7992a3b7f7365c2b839
it-green <ryo.ctuad@gmail.com>
2023-11-27 18:58:30 +0900

    fix: functions.php

--- theme/functions.php
+++ theme/functions.php
@@ -1,9 +1,11 @@
 <?php
 // cssとjsの反映
 add_action('wp_enqueue_scripts', 'get_styles_script');
-function get_styles_script() {
+function get_styles_script()
+{
   // import style
   wp_enqueue_style('main', get_template_directory_uri() . "/assets/css/main.css", array(), false, 'all');
   // import google fonts
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap', array(), false, 'all');
+  wp_enqueue_style('google-fonts-noto-sans', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap', array(), false, 'all');
+  wp_enqueue_style('google-fonts-robot', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap', array(), false, 'all');
 }
