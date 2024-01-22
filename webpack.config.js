1c3e878ec8d7fc677c649d6fd5d8ac17dc6d6bae
it-green <ryo.ctuad@gmail.com>
2024-01-22 09:56:54 +0900

    fix: scss で画像ファイルをリンクした場合の処理を変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const WebpackWatchedGlobEntries = require("webpack-watched-glob-entries-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
+  mode: "development",
+  devtool: "source-map",
   // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: WebpackWatchedGlobEntries.getEntries(
-    [
-      path.resolve(__dirname, 'theme/js/entry/**/*.js'),
-      path.resolve(__dirname, 'theme/scss/style.scss'),
-    ],
-  ),
+  entry: WebpackWatchedGlobEntries.getEntries([
+    path.resolve(__dirname, "theme/js/entry/**/*.js"),
+    path.resolve(__dirname, "theme/scss/style.scss"),
+  ]),
 
   // ファイルの出力設定
   output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, './theme/assets/'),
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "./theme/assets/"),
   },
 
   module: {
@@ -29,10 +27,10 @@ module.exports = {
       {
         test: /\.css/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
-          }
+            loader: "css-loader",
+          },
         ],
       },
       // sassのコンパイル設定
@@ -40,10 +38,10 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               url: true,
               sourceMap: true,
@@ -51,33 +49,50 @@ module.exports = {
             },
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [
-                  ["autoprefixer", { grid: true }],
-                ],
+                plugins: [["autoprefixer", { grid: true }]],
               },
             },
           },
           {
-            loader: 'resolve-url-loader',
+            loader: "resolve-url-loader",
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: true,
-            }
+            },
           },
         ],
       },
-      // 画像をbase64にエンコーディング
       {
         // 対象となるファイルの拡張子
-        test: /\.(gif|webp|png|jpg|eot|wof|woff|ttf|svg)$/,
-        // 画像をBase64として取り込む
-        type: "asset/inline",
-      }
+        test: /\.(gif|svg|eot|wof|woff|ttf)$/,
+        type: "asset",
+        generator: {
+          filename: "css/resources/[hash][ext][query]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024, // 4kb
+          },
+        },
+      },
+      {
+        // 対象となるファイルの拡張子
+        test: /\.(webp|png|jpg|jpeg)$/,
+        type: "asset",
+        generator: {
+          filename: "css/resources/[hash].webp[query]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024, // 4kb
+          },
+        },
+      },
     ],
   },
 
@@ -93,28 +108,33 @@ module.exports = {
 
   plugins: [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        'css/**/*',
-        'js/**/*',
-      ],
+      cleanOnceBeforeBuildPatterns: ["css/**/*", "js/**/*"],
     }),
 
     new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css'
+      filename: "css/[name].css",
     }),
 
     new WebpackWatchedGlobEntries(),
 
     new BrowserSyncPlugin({
-      host: 'localhost',
-      files: ['./**/*'],
+      host: "localhost",
+      files: ["./**/*"],
       port: 3000,
       proxy: {
-        target: 'http://localhost:10011',
+        target: "http://localhost:10011",
       },
       watchOptions: {
-        ignored: ['webpack.config.js', 'node_modules', 'sql', 'package.json', 'package-lock.json', 'php.ini', 'readme.md'],
+        ignored: [
+          "webpack.config.js",
+          "node_modules",
+          "sql",
+          "package.json",
+          "package-lock.json",
+          "php.ini",
+          "readme.md",
+        ],
       },
       open: false,
       ghostMode: {
@@ -122,7 +142,7 @@ module.exports = {
         forms: false,
         scroll: false,
       },
-      logLevel: 'debug',
+      logLevel: "debug",
     }),
   ],
 };
